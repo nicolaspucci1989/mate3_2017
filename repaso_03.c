f7494a46fca6d74801f28b3695bebe5e53fbb135
Nicolas Pucci <nicolaspucci1989@gmail.com>
2017-03-10 13:56:54 -0300

    segrega el vector en pares impares

--- repaso_03.c
+++ repaso_03.c
@@ -11,25 +11,38 @@ void ingreso(int v[])
 }
 
 
-int compPar(int a)
+void intercambio(int *a, int *b)
 {
-  return a%2 == 0;
+  int temp;
+  temp = *a;
+  *a = *b;
+  *b = temp;
 }
 
 
-int compImpar(int a)
+void segregarParesImpares(int v[])
 {
-  return a%2 != 0;
+  int izquierda = 0, derecha = ELEMENTOS - 1;
+  while(izquierda < derecha){
+    while (v[izquierda]%2 == 0 && izquierda < derecha )
+      izquierda++;
+
+    while (v[derecha]%2 == 1 && izquierda < derecha)
+      derecha--;
+
+    if(izquierda < derecha){
+      intercambio(&v[izquierda], &v[derecha]);
+      izquierda++;
+      derecha--;
+    }
+  }
 }
-
-
-void impresion(int v[], int (*comp)(int a))
+void impresion(int v[])
 {
   int i;
 
   for(i=0; i<ELEMENTOS; i++){
-    if((*comp)(v[i]))
-      printf("%d ", v[i]);
+    printf("%d ", v[i]);
   }
   putchar('\n');
 }
@@ -38,11 +51,10 @@ void impresion(int v[], int (*comp)(int a))
 int main()
 {
   int v[ELEMENTOS];
+  printf("Ingrese %d numeros\n", ELEMENTOS);
   ingreso(v);
-  printf("%s", "Pares: ");
-  impresion(v, compPar);
-  printf("%s", "Impares: ");
-  impresion(v, compImpar);
+  segregarParesImpares(v);
+  impresion(v);
 
   return 0;
 }
