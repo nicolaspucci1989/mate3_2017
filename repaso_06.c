9dcef9397ebed6350b908b0c2e2f732ee87d28c2
Nicolas Pucci <nicolaspucci1989@gmail.com>
2017-03-23 19:42:47 -0300

    completa ej 6

--- repaso_06.c
+++ repaso_06.c
@@ -1,10 +1,44 @@
 //ingreso de 15 numeros en un vector
 // mostrar los de indice par.
-#include <stiod.h>
+#include <stdio.h>
 #include <stdlib.h>
+#define NUMEROS 5
+
+void ingreso(int v[])
+{
+  int i;
+  for(i=0; i<NUMEROS; i++){
+    scanf("%d", &v[i]);
+  }
+}
+
+
+void impresion(int v[])
+{
+  int i;
+  for(i=0; i<NUMEROS; i++){
+    if(i%2 == 0){
+      printf("%d ", v[i]);
+    }
+  }
+  putchar('\n');
+}
+
 
 int main()
 {
-  
+  int vector[NUMEROS];
+
+
+  printf("Ingrese %d numeros\n", NUMEROS);
+
+
+  ingreso(vector);
+  fflush(stdin);
+  impresion(vector);
+
+
+  puts("Presione enter para continuar");
+  getchar();
   return 0;
 }
