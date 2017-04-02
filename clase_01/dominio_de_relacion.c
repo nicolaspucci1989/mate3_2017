eedd317c9be216c228f47cf0a78135bc3be78908
Nicolas Pucci <nicolaspucci1989@gmail.com>
2017-04-01 23:32:34 -0300

    imprime dominio

--- clase_01/dominio_de_relacion.c
+++ clase_01/dominio_de_relacion.c
@@ -4,36 +4,24 @@
 
 
 int main(){
-  int i;
+  int i, j;
   int conjuntoA[] = {1, 2, 3, 8};
   // int conjuntoA[ELEMENTOS_A];
   char conjuntoB[] = {'a' ,'b', 'j', 'k'};
   // char conjuntoB[ELEMENTOS_B];
   struct relacion rel[]= {{1,'b'}, {8, 'k'}};
   // struct relacion rel[ELEMENTOS_RELACION];
-  // int dominio[ELEMENTOS_A] = {-1, -1, -1, -1};
+  int dominio[ELEMENTOS_A] = {-1, -1, -1, -1};
   // char imagen[ELEMENTOS_B] = {-1, -1, -1, -1};
 
-  for(i=0; i<ELEMENTOS_A; i++){
-    if(estaEnDominio(conjuntoA[i], rel, conjuntoB))
-    // if(estaEnLaRelacionX(conjuntoA[0], rel) != -1)
-      printf("Elemento %d esta en el dominio\n", conjuntoA[i]);
-    else
-      printf("Elemento %d no esta en el dominio\n", conjuntoA[i]);
+  for(i=0, j=0; i<ELEMENTOS_A; i++){
+    if(estaEnDominio(conjuntoA[i], rel, conjuntoB)){
+      dominio[j] = conjuntoA[i];
+      j++;
+    }
   }
-  // printf("Ingrese %d elementos del conjunto A\n", ELEMENTOS_A);
-  // ingresoConjuntoA(conjuntoA);
-  // fflush(stdin);
-  //
-  //
-  // printf("Ingrese %d elementos del conjunto B\n", ELEMENTOS_B);
-  // ingresoConjuntoB(conjuntoB);
-  //
-  //
-  // printf("ingrese %d elementos de la relacion", ELEMENTOS_RELACION);
-  // ingresoRelacion(rel);
 
-
+  imprimirDominio(dominio);
 
 
   fflush(stdin);
