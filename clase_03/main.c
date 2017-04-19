5805c662cf57fadda60892155ccb8e87fd64348f
Nicolas Pucci <nicolaspucci1989@gmail.com>
2017-04-18 21:11:37 -0300

    agrega clase 3

---
+++ clase_03/main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main()
+{
+  int esPrimo = 0;
+  int i;
+  int divisor = 1;
+  char *cartel[] = {"Es primo", "No es primo"};
+  int numero = 98;
+
+  numero = (int) sqrt(numero);
+  numero --;
+
+  for(i=2; i<numero; i++){
+    if(numero%i == 0){
+      esPrimo = 1;
+      divisor = i;
+      break;
+    }
+  }
+
+  printf("%s, divisor: %d\n", cartel[esPrimo], divisor);
+
+
+  puts("Presione enter");
+  getchar();
+  return 0;
+}
