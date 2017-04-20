2aa59535fc9dccc8a105351a07f6e9c4d4bd3a2c
Nicolas Pucci <nicolaspucci1989@gmail.com>
2017-04-20 20:10:02 -0300

    cambios menores

--- clase_03/cirba_era.c
+++ clase_03/cirba_era.c
@@ -2,10 +2,29 @@
 //con el metodo de la criba
 #include <stdio.h>
 #include <stdlib.h>
+#define FILAS 10
+#define COLUMNS 10
 
 
 int main()
 {
+  int i,j,k=1;
+  int matriz[FILAS][COLUMNS];
+
+
+  for(i=0; i<FILAS; i++){
+    for(j=0; j<COLUMNS; j++){
+      matriz[i][j] = k;
+      k++;
+    }
+  }
+
+  for(i=0; i<FILAS; i++){
+    for(j=0; j<COLUMNS; j++){
+      printf("%d ", matriz[i][j]);
+    }
+    putchar('\n');
+  }
 
 
   puts("Presione enter para continuar");
