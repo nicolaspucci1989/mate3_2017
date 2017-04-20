a043f5a1aed12a170eea0ed1a3237ae39d798e9e
Nicolas Pucci <nicolaspucci1989@gmail.com>
2017-04-20 20:04:33 -0300

    agrega archivo criba

---
+++ clase_03/cirba_era.c
@@ -0,0 +1,14 @@
+//en una matriz de 10 x 10 mostrar solo los primos
+//con el metodo de la criba
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main()
+{
+
+
+  puts("Presione enter para continuar");
+  getchar();
+  return 0;
+}
