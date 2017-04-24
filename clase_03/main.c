7e9345556b7252efa5c49a75d079e635c901efba
Nicolas Pucci <nicolaspucci1989@gmail.com>
2017-04-24 09:54:51 -0300

    mejora impresion

--- clase_03/main.c
+++ clase_03/main.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define PRIMO 1
+#define COMPUESTO 0
+
 
 
 void primos(int v[], int n);
@@ -7,20 +10,22 @@ void primos(int v[], int n);
 
 int main()
 {
-    int v[100];
-    int i, n=100;
+    int i,k,n=100;
+    int v[n];
 
     for(i=0; i<n; i++){
-      v[i]=1;
+      v[i]=PRIMO;
     }
 
 
     primos(v,n);
 
 
-    for(i=0; i<n; i++){
-      if(v[i]==1)
-        printf("%d\n", i);
+    for(i=0, k=1; i<n; i++){
+      if(v[i]==PRIMO){
+        printf("%-4.d%c", i,(k%5)?' ':'\n');
+        k++;
+      }
     }
     return 0;
 }
@@ -29,8 +34,8 @@ void primos(int v[], int n)
 {
   int i,j;
 
-  v[0]=v[1]=0;
+  v[0]=v[1]=COMPUESTO;
   for(i=2; i*i<n; i++)
     for(j=i*i; j<n; j+=i)
-      v[j]=0;
+      v[j]=COMPUESTO;
 }
