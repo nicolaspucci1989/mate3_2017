f9672da7fab9d149c428d255b0b94cfb95f23d02
Nicolas Pucci <nicolaspucci1989@gmail.com>
2017-04-24 09:46:57 -0300

    propia criba

--- clase_03/main.c
+++ clase_03/main.c
@@ -1,28 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void primos(int n, int v[]);
+
+void primos(int v[], int n);
+
 
 int main()
 {
-    int i, n = 100; // find the primes up to 500000
-    int v[n];
-    primos(n, v);
-    for (i=0;i<n;i++)
-        if (v[i] == 1)
-            printf("%d\n",i); // this just prints out each value if it's Prime
+    int v[100];
+    int i, n=100;
 
+    for(i=0; i<n; i++){
+      v[i]=1;
+    }
+
+
+    primos(v,n);
+
+
+    for(i=0; i<n; i++){
+      if(v[i]==1)
+        printf("%d\n", i);
+    }
     return 0;
 }
 
-void primos(int n, int v[])
+void primos(int v[], int n)
 {
-    int i, j, cuadrado;
-    cuadrado = n*n;
-    for(i=0;i<n;i++)
-        v[i]=1; // we initialize the sieve list to all 1's (True)
-    v[0]=0,v[1]=0; // Set the first two numbers (0 and 1) to 0 (False)
-    for (i=2;i<cuadrado;i++) // loop through all the numbers up to the sqrt(n)
-        for (j=i*i;j<n;j+=i) // mark off each factor of i by setting it to 0 (False)
-            v[j] = 0;
+  int i,j;
+
+  v[0]=v[1]=0;
+  for(i=2; i*i<n; i++)
+    for(j=i*i; j<n; j+=i)
+      v[j]=0;
 }
