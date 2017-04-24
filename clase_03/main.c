b44d666a7380265fac922f23ba175cc3e691075c
Nicolas Pucci <nicolaspucci1989@gmail.com>
2017-04-24 09:25:59 -0300

    cirba ok

--- clase_03/main.c
+++ clase_03/main.c
@@ -1,29 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
+
+void primos(int n, int v[]);
 
 int main()
 {
-  int esPrimo = 0;
-  int i;
-  int divisor = 1;
-  char *cartel[] = {"Es primo", "No es primo"};
-  int numero = 67;
+    int i, n = 100; // find the primes up to 500000
+    int v[n];
+    primos(n, v);
+    for (i=0;i<n;i++)
+        if (v[i] == 1)
+            printf("%d\n",i); // this just prints out each value if it's Prime
 
-  numero = ((int) sqrt(numero)) - 1;
+    return 0;
+}
 
-  for(i=2; i<numero; i++){
-    if(numero%i == 0){
-      esPrimo = 1;
-      divisor = i;
-      break;
-    }
-  }
-
-  printf("%s, divisor: %d\n", cartel[esPrimo], divisor);
-
-
-  puts("Presione enter");
-  getchar();
-  return 0;
+void primos(int n, int v[])
+{
+    int i, j, cuadrado;
+    cuadrado = n*n;
+    for(i=0;i<n;i++)
+        v[i]=1; // we initialize the sieve list to all 1's (True)
+    v[0]=0,v[1]=0; // Set the first two numbers (0 and 1) to 0 (False)
+    for (i=2;i<cuadrado;i++) // loop through all the numbers up to the sqrt(n)
+        for (j=i*i;j<n;j+=i) // mark off each factor of i by setting it to 0 (False)
+            v[j] = 0;
 }
