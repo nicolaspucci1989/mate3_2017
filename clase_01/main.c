23fdc69e6e99e0b271be1ff954520b0637ac4b06
Nicolas Pucci <nicolaspucci1989@gmail.com>
2017-04-26 11:38:19 -0300

    agrega funciones dominio imagen

--- clase_01/main.c
+++ clase_01/main.c
@@ -2,20 +2,10 @@
 #include <stdlib.h>
 #include "header.h"
 
-
-int main(){
-
-
-  int i, j, k, h ,n=4;
-  int conjuntoA[] = {1, 2, 3, 8};
-  // int conjuntoA[ELEMENTOS_A];
-  char conjuntoB[] = {'a' ,'b', 'j', 'k'};
-  // char conjuntoB[ELEMENTOS_B];
-  struct relacion rel[]= {{1,'b'}, {8, 'k'}};
-  // struct relacion rel[ELEMENTOS_RELACION];
-  int dominio[ELEMENTOS_A] = {-1, -1, -1, -1};
-  char imagen[ELEMENTOS_B] = {-1, -1, -1, -1};
-
+void obtenerDominio(int n, int dominio[], int conjuntoA[],
+                  char conjuntoB[], struct relacion rel[])
+{
+  int i,h,j,k;
   for(i=0,h=0;i<n;i++)
     for(j=0;j<n;j++)
       if(conjuntoA[i]==rel[j].x)
@@ -24,7 +14,13 @@ int main(){
             dominio[h]=conjuntoA[i];
             h++;
           }
-  imprimirDominio(dominio);
+}
+
+
+void obtenerImagen(int n, char imagen[], char conjuntoB[],
+                  int conjuntoA[], struct relacion rel[])
+{
+  int i,h,j,k;
 
   for(i=0,h=0;i<n;i++)
     for(j=0;j<n;j++)
@@ -34,7 +30,26 @@ int main(){
             imagen[h]=conjuntoB[i];
             h++;
           }
+}
 
+
+int main(){
+
+
+  int n=4;
+  int conjuntoA[] = {1, 2, 3, 8};
+  // int conjuntoA[ELEMENTOS_A];
+  char conjuntoB[] = {'a' ,'b', 'j', 'k'};
+  // char conjuntoB[ELEMENTOS_B];
+  struct relacion rel[]= {{1,'b'}, {8, 'k'}};
+  // struct relacion rel[ELEMENTOS_RELACION];
+  int dominio[] = {-1, -1, -1, -1};
+  char imagen[] = {-1, -1, -1, -1};
+
+  obtenerDominio(n,dominio,conjuntoA,conjuntoB,rel);
+  imprimirDominio(dominio);
+  obtenerImagen(n,imagen,conjuntoB,conjuntoA,rel);
   imprimirImagen(imagen);
+  
   return 0;
 }
