6ca44e7d7e39dc87ee7bac3ad69b5fcdbfd4c098
Nicolas Pucci <nicolaspucci1989@gmail.com>
2017-04-26 01:05:04 -0300

    agrega imagen

--- clase_01/main.c
+++ clase_01/main.c
@@ -14,7 +14,7 @@ int main(){
   struct relacion rel[]= {{1,'b'}, {8, 'k'}};
   // struct relacion rel[ELEMENTOS_RELACION];
   int dominio[ELEMENTOS_A] = {-1, -1, -1, -1};
-  // char imagen[ELEMENTOS_B] = {-1, -1, -1, -1};
+  char imagen[ELEMENTOS_B] = {-1, -1, -1, -1};
 
   for(i=0,h=0;i<n;i++)
     for(j=0;j<n;j++)
@@ -25,5 +25,16 @@ int main(){
             h++;
           }
   imprimirDominio(dominio);
+
+  for(i=0,h=0;i<n;i++)
+    for(j=0;j<n;j++)
+      if(conjuntoB[i]==rel[j].y)
+        for(k=0;k<n;k++)
+          if(conjuntoA[k]==rel[j].x){
+            imagen[h]=conjuntoB[i];
+            h++;
+          }
+
+  imprimirImagen(imagen);
   return 0;
 }
