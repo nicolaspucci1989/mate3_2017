f509ce5f869f55a2d63646b2336a4736a7ff83c6
Nicolas Pucci <nicolaspucci1989@gmail.com>
2017-05-04 20:15:43 -0300

    agregando primos a mezcla de suc

--- clase_04/mezcla_de_sucesiones.c
+++ clase_04/mezcla_de_sucesiones.c
@@ -1,39 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define PRIMO 1
+#define COMPUESTO 0
 
-void primos(int v[], int n);
-int fibo(int n);
+
+
+void primos(int v[], int tam);
+
 
 int main()
 {
+    int i;
+    int n=10;
+    int v[n];
 
+    primos(v,n);
 
-  puts("Presione enter para continuar");
-  getchar();
-  return 0;
+    for (i = 0; i < n; i++) {
+      printf("%d ", v[i]);
+    }
+    return 0;
 }
 
-
-void primos(int v[], int n)
+void primos(int salida[], int tam)
 {
-  int i,j;
+  int i,k,j,n=100;
+  int v[n];
+
+  for(i=0; i<n; i++){
+    v[i]=PRIMO;
+  }
 
   v[0]=v[1]=COMPUESTO;
   for(i=2; i*i<n; i++)
     for(j=i*i; j<n; j+=i)
       v[j]=COMPUESTO;
-}
 
-int fibo(int n)
-{
-  int res;
-
-  if(n == 0)
-    res = 0;
-  else if(n == 1)
-    res = 1;
-  else
-    res = (fibo(n-1) + fibo(n-2));
-
-  return res;
+  for(i=0, k=0; k<tam; i++){
+    if(v[i]==PRIMO){
+      salida[k]=i;
+      k++;
+    }
+  }
 }
