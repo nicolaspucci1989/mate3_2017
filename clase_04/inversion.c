a062e77f09f55d8f881ba9c8a0319002d1f1eb06
Nicolas Pucci <nicolaspucci1989@gmail.com>
2017-05-04 19:20:10 -0300

    Agregando clase 4 ej 1, interes

---
+++ clase_04/inversion.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main()
+{
+  float inversion, tasa=.3;
+  int anios, i;
+
+  printf("%s\n", "Ingrese monto de inversion.");
+  scanf("%f", &inversion);
+  fflush(stdin);
+
+  printf("%s\n", "Ingrese cantidad de anos.");
+  scanf("%d", &anios);
+  fflush(stdin);
+
+
+  for(i=0;i<anios;i++){
+    inversion+=inversion*tasa;
+    printf("Anio %d: %.2f\n", i+1, inversion);
+  }
+
+  puts("Presione enter para continuar");
+  getchar();
+  return 0;
+}
