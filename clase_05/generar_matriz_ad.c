1e17124ed718b52f5c4be1262a71861ad1b5a65d
Nicolas Pucci <nicolaspucci1989@gmail.com>
2017-05-11 20:27:40 -0300

    agregando rand a matriz ad

--- clase_05/generar_matriz_ad.c
+++ clase_05/generar_matriz_ad.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <time.h>
 
 int main()
 {
+  srand(time(NULL));
+  int r=rand()%2;
   int n=5;
   int matriz[n][n];
   int i,j;
@@ -13,11 +15,15 @@ int main()
       matriz[i][j]=-1;
 
   for(i=0;i<n;i++)
-    matriz[i][i]=0;
+    matriz[i][i]=r;
 
+  //Recorro la diagonal superiro
   for(i=0;i<n;i++)
-    for(j=i+1;j<n;j++)
-      matriz[i][j]=1;
+    for(j=i+1;j<n;j++){
+      r=rand()%2;
+      matriz[i][j]=r;
+      matriz[j][i]=r;
+    }
 
 
   for(i=0;i<n;i++){
